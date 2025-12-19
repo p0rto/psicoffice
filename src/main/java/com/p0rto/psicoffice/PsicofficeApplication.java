a3ac8a770fff16ce62e975affca7bfc9fbef9908
Gabriel Romualdo Porto <gabrielr.porto@pmpa.ad>
2025-12-18 22:58:13 -0300

    first commit

---
+++ src/main/java/com/p0rto/psicoffice/PsicofficeApplication.java
@@ -0,0 +1,13 @@
+package com.p0rto.psicoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsicofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsicofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/p0rto/psicoffice/PsicofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p0rto.psicoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsicofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
